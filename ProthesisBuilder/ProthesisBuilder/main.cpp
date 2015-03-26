155e7569968c74d83e9b1a345818d509058541e5
borzenkov <vit-borzenkov@yandex.ru>
2015-03-26 19:12:32 +0300

    Переход на VS2013

--- ProthesisBuilder/ProthesisBuilder/main.cpp
+++ ProthesisBuilder/ProthesisBuilder/main.cpp
@@ -1,3 +1,5 @@
+#define _SCL_SECURE_NO_WARNINGS
+
 #include <CGAL/Cartesian.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/IO/Polyhedron_iostream.h>
@@ -29,7 +31,7 @@ typedef Kernel::Vector_3 Vector_3;
 typedef Polyhedron::HalfedgeDS HalfedgeDS;
 typedef Kernel::Ray_3 Ray;
 
-typedef CGAL::AABB_polyhedron_triangle_primitive<Kernel,Polyhedron> Primitive;
+typedef CGAL::AABB_polyhedron_triangle_primitive<Kernel, Polyhedron> Primitive;
 typedef CGAL::AABB_traits<Kernel, Primitive> Traits;
 typedef CGAL::AABB_tree<Traits> Tree;
 typedef boost::optional< Tree::Intersection_and_primitive_id<Ray>::Type > Ray_intersection;
@@ -41,27 +43,27 @@ class polyhedron_builder : public CGAL::Modifier_base<HDS> {
 public:
 	std::vector<double> &coords;
 	std::vector<int>    &tris;
-	polyhedron_builder( std::vector<double> &_coords, std::vector<int> &_tris ) : coords(_coords), tris(_tris) {}
-	void operator()( HDS& hds) {
+	polyhedron_builder(std::vector<double> &_coords, std::vector<int> &_tris) : coords(_coords), tris(_tris) {}
+	void operator()(HDS& hds) {
 		typedef typename HDS::Vertex   Vertex;
-        typedef typename Vertex::Point Point;
+		typedef typename Vertex::Point Point;
 		// create a cgal incremental builder
-        CGAL::Polyhedron_incremental_builder_3<HDS> B( hds, true);
-        B.begin_surface( coords.size()/3, tris.size()/3 );
+		CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
+		B.begin_surface(coords.size() / 3, tris.size() / 3);
 		// add the polyhedron vertices
-		for( int i=0; i<(int)coords.size(); i+=3 ){
-			B.add_vertex( Point( coords[i+0], coords[i+1], coords[i+2] ) );
+		for (int i = 0; i<(int)coords.size(); i += 3){
+			B.add_vertex(Point(coords[i + 0], coords[i + 1], coords[i + 2]));
 		}
 		// add the polyhedron triangles
-		for( int i=0; i<(int)tris.size(); i+=3 ){
+		for (int i = 0; i<(int)tris.size(); i += 3){
 			B.begin_facet();
-			B.add_vertex_to_facet( tris[i+0] );
-			B.add_vertex_to_facet( tris[i+1] );
-			B.add_vertex_to_facet( tris[i+2] );
+			B.add_vertex_to_facet(tris[i + 0]);
+			B.add_vertex_to_facet(tris[i + 1]);
+			B.add_vertex_to_facet(tris[i + 2]);
 			B.end_facet();
 		}
 		// finish up the surface
-        B.end_surface();
+		B.end_surface();
 	}
 };
 
@@ -69,11 +71,11 @@ int read_polyhedron_from_off_file(const char* input_filename, Polyhedron &mesh)
 {
 	std::ifstream stream(input_filename);
 	stream >> mesh;
-	if(!stream || !mesh.is_valid() || mesh.empty())
-    {
-        std::cerr << "Error: cannot read OFF file " << input_filename << std::endl;
-        return 1;
-    }
+	if (!stream || !mesh.is_valid() || mesh.empty())
+	{
+		std::cerr << "Error: cannot read OFF file " << input_filename << std::endl;
+		return 1;
+	}
 	return 0;
 }
 
@@ -81,19 +83,18 @@ void get_facets_with_equal_z_and_specified_tolerance(double z, double tolerance,
 {
 	std::vector<double> coords;
 	std::vector<int>    tris;
-	int counter_of_points_with_equal_z = 0, index_of_point = -1; 
-	for ( Facet_iterator f = mesh.facets_begin(); f != mesh.facets_end(); ++f) {
+	int counter_of_points_with_equal_z = 0, index_of_point = -1;
+	for (Facet_iterator f = mesh.facets_begin(); f != mesh.facets_end(); ++f) {
 		counter_of_points_with_equal_z = 0;
-		Halfedge_facet_circulator v = f->facet_begin();	
-		 do
+		Halfedge_facet_circulator v = f->facet_begin();
+		do
 		{
-			if( ( v->vertex()->point().z() > ( z - tolerance ) ) && ( v->vertex()->point().z() < z + tolerance ) )
+			if ((v->vertex()->point().z() > (z - tolerance)) && (v->vertex()->point().z() < z + tolerance))
 			{
 				counter_of_points_with_equal_z++;
 			}
-		}
-		while( ++v != f->facet_begin());
-		if(counter_of_points_with_equal_z == 3) 
+		} while (++v != f->facet_begin());
+		if (counter_of_points_with_equal_z == 3)
 		{
 			v = f->facet_begin();
 			do
@@ -102,12 +103,11 @@ void get_facets_with_equal_z_and_specified_tolerance(double z, double tolerance,
 				coords.push_back(v->vertex()->point().y());
 				coords.push_back(v->vertex()->point().z());
 				tris.push_back(++index_of_point);
-			}
-			while( ++v != f->facet_begin());
+			} while (++v != f->facet_begin());
 		}
 	}
-	polyhedron_builder<HalfedgeDS> builder( coords, tris );
-	rezult.delegate( builder );
+	polyhedron_builder<HalfedgeDS> builder(coords, tris);
+	rezult.delegate(builder);
 }
 
 void create_big_triangle(Polyhedron &big_triangle)
@@ -126,8 +126,8 @@ void create_big_triangle(Polyhedron &big_triangle)
 	tris.push_back(0);
 	tris.push_back(1);
 	tris.push_back(2);
-	polyhedron_builder<HalfedgeDS> builder( coords, tris );
-	big_triangle.delegate( builder );
+	polyhedron_builder<HalfedgeDS> builder(coords, tris);
+	big_triangle.delegate(builder);
 }
 
 void get_projection_surface(Polyhedron &surface_a, Polyhedron &surface_b, Vector_3 &vector, Polyhedron &rezult)
@@ -140,7 +140,7 @@ void get_projection_surface(Polyhedron &surface_a, Polyhedron &surface_b, Vector
 	Tree tree(surface_b.facets_begin(), surface_b.facets_end());
 
 	Point_iterator pi = surface_a.points_begin();
-	for (Vertex_iterator vi = surface_a.vertices_begin(); vi != surface_a.vertices_end(); vi++) 
+	for (Vertex_iterator vi = surface_a.vertices_begin(); vi != surface_a.vertices_end(); vi++)
 	{
 		Ray ray(vi->point(), vector);
 		// computes first encountered intersection with segment query
@@ -149,17 +149,17 @@ void get_projection_surface(Polyhedron &surface_a, Polyhedron &surface_b, Vector
 		std::vector<Ray_intersection> all_intersections;
 		std::vector<Point_3> all_intersection_points;
 		std::vector<double> distances;
-		if(tree.do_intersect(ray))
+		if (tree.do_intersect(ray))
 		{
 			tree.all_intersections(ray, std::back_inserter(all_intersections));
-			for(int i = 0; i < all_intersections.size(); i++)
+			for (int i = 0; i < all_intersections.size(); i++)
 			{
 				all_intersection_points.push_back(*boost::get<Point_3>(&(all_intersections[i]->first)));
 			}
-			
-			for(int i = 0; i < all_intersection_points.size(); i++)
+
+			for (int i = 0; i < all_intersection_points.size(); i++)
 			{
-				distances.push_back(pow(all_intersection_points[i].x() - vi->point().x(), 2) + 
+				distances.push_back(pow(all_intersection_points[i].x() - vi->point().x(), 2) +
 					pow(all_intersection_points[i].y() - vi->point().y(), 2) +
 					pow(all_intersection_points[i].z() - vi->point().z(), 2));
 			}
@@ -171,7 +171,7 @@ void get_projection_surface(Polyhedron &surface_a, Polyhedron &surface_b, Vector
 			//std::cout << *pi << std::endl;
 			Point_iterator end = pi;
 			end++;
-			std::transform(pi, end, pi, translation); 
+			std::transform(pi, end, pi, translation);
 			//std::cout << "after affine transform" << std::endl;
 			//std::cout << *pi << std::endl;
 			//std::transform(pi, pi, pi, translation);
@@ -179,7 +179,7 @@ void get_projection_surface(Polyhedron &surface_a, Polyhedron &surface_b, Vector
 			coords.push_back(all_intersection_points[index_of_closest_point].y());
 			coords.push_back(all_intersection_points[index_of_closest_point].z());
 			tris.push_back(i++);*/
-		}	
+		}
 		pi++;
 	}
 	/*polyhedron_builder<HalfedgeDS> builder( coords, tris );
@@ -195,17 +195,17 @@ int read_my_OFF(const char* file_name, Polyhedron &rezult)
 	std::vector<double> coords;
 	std::vector<int> tris;
 	FILE *f;
-	if((f = fopen(file_name, "r")) != NULL)
+	if ((f = fopen(file_name, "r")) != NULL)
 	{
 		fscanf(f, "%s%d%d%d", format, &number_of_points, &number_of_faces, &number_of_lines);
-		for(int i = 0; i < number_of_points; i++)
+		for (int i = 0; i < number_of_points; i++)
 		{
 			fscanf(f, "%f%f%f", &x, &y, &z);
 			coords.push_back(x);
 			coords.push_back(y);
 			coords.push_back(z);
 		}
-		for(int i = 0; i < number_of_faces; i++)
+		for (int i = 0; i < number_of_faces; i++)
 		{
 			fscanf(f, "%d%d%d%d", &number_of_composing_points, &index1, &index2, &index3);
 			tris.push_back(index1);
@@ -214,8 +214,8 @@ int read_my_OFF(const char* file_name, Polyhedron &rezult)
 		}
 	}
 	fclose(f);
-	polyhedron_builder<HalfedgeDS> builder( coords, tris );
-	rezult.delegate( builder );
+	polyhedron_builder<HalfedgeDS> builder(coords, tris);
+	rezult.delegate(builder);
 	return 0;
 }
 
@@ -226,27 +226,27 @@ int main(int argc, char * argv[])
 	std::vector<Point_3> points;
 	for(int i = 0; i < 10; i++)
 	{
-		Point_3 point(i, i, i);
-		points.push_back(point);
-		std::cout << points[i] << std::endl;
+	Point_3 point(i, i, i);
+	points.push_back(point);
+	std::cout << points[i] << std::endl;
 	}
 	std::transform(points.begin(), ++points.begin(), points.begin(), translation);
 	for(int i = 0; i < 10; i++)
 	{
-		std::cout << points[i] << std::endl;
+	std::cout << points[i] << std::endl;
 	}*/
-    // decode parameters
-    if (argc-1 != 2)
-    {
-        std::cerr << "Usage: " << argv[0] << " input_file.off" << std::endl;
-        return(EXIT_FAILURE);
-    }
+	// decode parameters
+	if (argc - 1 != 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " input_file.off" << std::endl;
+		return(EXIT_FAILURE);
+	}
 
-    // File name is:
-    const char* input_filename_prothesis  = argv[1];
-	const char* input_filename_jaw  = argv[2];
+	// File name is:
+	const char* input_filename_prothesis = argv[1];
+	const char* input_filename_jaw = argv[2];
 
-    // Read the mesh
+	// Read the mesh
 	Polyhedron prothesis;
 	read_polyhedron_from_off_file(input_filename_prothesis, prothesis);
 
@@ -266,7 +266,7 @@ int main(int argc, char * argv[])
 	os << big_triangle;
 	os.close();
 
-	Vector_3 vector(0, 0, -1);
+	Vector_3 vector(0.2, 0.2, -0.9);//rename
 	Polyhedron rezult;
 	get_projection_surface(prothesis_part, jaw, vector, rezult);
 
