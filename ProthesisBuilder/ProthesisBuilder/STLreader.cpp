00d993dc7036405c9312e9060025bf9ba49ea9ad
borzenkov <vit-borzenkov@yandex.ru>
2015-04-09 18:36:39 +0300

    STL reader is added

---
+++ ProthesisBuilder/ProthesisBuilder/STLreader.cpp
@@ -0,0 +1,161 @@
+#include <CGAL/Modifier_base.h>
+#include <CGAL/Polyhedron_incremental_builder_3.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/Polyhedron_traits_with_normals_3.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <fstream>
+#include <string>
+
+#include <boost/tuple/tuple.hpp>
+
+typedef CGAL::Cartesian<double> Kernel;
+typedef Kernel::Point_3 Point_3;
+typedef std::vector<Point_3> Points_3;
+typedef boost::tuple<int, int, int> Facet;
+typedef std::vector<Facet> Surface;
+typedef CGAL::Polyhedron_traits_with_normals_3<Kernel> Polyhedron_traits_with_normals_3;
+typedef CGAL::Polyhedron_3<Polyhedron_traits_with_normals_3> Polyhedron;
+
+template <class HDS>
+class Build_from_stl : public CGAL::Modifier_base<HDS> {
+	std::istream& is;
+	int counter;
+	Points_3 meshPoints;
+	Surface mesh;
+
+public:
+
+	std::string name, color;
+
+
+	Build_from_stl(std::istream& is_)
+		: is(is_), counter(0)
+	{}
+
+	void operator()(HDS& hds) {
+		if (!read(is, meshPoints, mesh)) return;
+
+		CGAL::Polyhedron_incremental_builder_3<Polyhedron::HalfedgeDS> B(hds);
+		B.begin_surface(meshPoints.size(), mesh.size());
+		typedef typename Points_3::size_type size_type;
+
+		for (size_type i = 0; i < meshPoints.size(); i++){
+			B.add_vertex(meshPoints[i]);
+		}
+		for (size_type i = 0; i < mesh.size(); i++){
+			B.begin_facet();
+			B.add_vertex_to_facet(mesh[i].template get<0>());
+			B.add_vertex_to_facet(mesh[i].template get<1>());
+			B.add_vertex_to_facet(mesh[i].template get<2>());
+			B.end_facet();
+		}
+		if (B.error())
+		{
+			std::cerr << "An error occured while creating a Polyhedron" << std::endl;
+			B.rollback();
+		}
+
+		B.end_surface();
+	}
+
+	bool
+		read(std::istream& input, Points_3& points, Surface& surface, int /*offset*/ = 0)
+	{
+			std::string s, solid("solid"), facet("facet"), outer("outer"), loop("loop"), vertex("vertex"), endloop("endloop"), endsolid("endsolid");
+
+			std::map<Point_3, int> vertex_index;
+			int index = 0;
+			int ijk[3];
+			Point_3 p;
+
+			input >> s;
+			if (s == solid){
+				std::getline(input, s);
+			}
+			else {
+				std::cerr << "We expect keyword 'solid'" << std::endl;
+				return false;
+			}
+
+			while (input >> s){
+				if (s == endsolid){
+					//std::cerr << "found endsolid" << std::endl;
+				}
+				else if (s == facet){
+					//std::cerr << "found facet" << std::endl;
+					std::getline(input, s); // ignore the normal
+					input >> s;
+					if (s != outer){
+						std::cerr << "Expect 'outer' and got " << s << std::endl;
+						return false;
+					}
+					input >> s;
+					if (s != loop){
+						std::cerr << "Expect 'loop' and got " << s << std::endl;
+						return false;
+					}
+					int count = 0;
+					do {
+						input >> s;
+						if (s == vertex){
+							//      std::cerr << "found vertex" << std::endl;
+							if (count < 3){
+								input >> p;
+								if (vertex_index.find(p) == vertex_index.end()){
+									ijk[count] = index;
+									vertex_index[p] = index++;
+									points.push_back(p);
+								}
+								else {
+									ijk[count] = vertex_index[p];
+								}
+								++count;
+							}
+							else {
+								std::cerr << "We can only read triangulated surfaces" << std::endl;
+								return false;
+							}
+						}
+					} while (s != endloop);
+
+					surface.push_back(boost::make_tuple(ijk[0], ijk[1], ijk[2]));
+				}
+			}
+			return true;
+		}
+
+};
+
+int readSTL(std::string file_name, Polyhedron &output)
+{
+	// Open file
+	std::ifstream in(file_name);
+	if (!in) {
+		std::cerr << "Error! Cannot open file " << file_name << std::endl;
+		return 0;
+	}
+
+	// Try to read STL file in a polyhedron
+	Build_from_stl<Polyhedron::HalfedgeDS> builder(in);
+	output.delegate(builder);
+	if (!output.is_valid()){
+		std::cerr << "Error: Invalid polyhedron" << std::endl;
+		return 0;
+	}
+
+	if (output.empty()){
+		return 0;
+	}
+}
+
+//void main()
+//{
+//	std::string file_name = "C:/FILES/ProthesisBuilder/OUTPUT/Plate.stl";
+//	Polyhedron plate;
+//	readSTL(file_name, plate);
+//
+//	std::ofstream os;
+//	os.open("C:/FILES/ProthesisBuilder/OUTPUT/STLreader_works_OK.off");
+//	os << plate;
+//}
--- ProthesisBuilder/ProthesisBuilder/main.cpp
+++ ProthesisBuilder/ProthesisBuilder/main.cpp
@@ -1,178 +1,178 @@
-// MedBox
-// Prothesis builder program
-// (c) Victor Borzenkov 2014
-
-#include "ProthesisInputData.h"
-#include "Prothesis.h"
-
-int main()
-{
-	// Create an object for passing inputs to Prothesis builder class
-	ProthesisInputData * inputs = new ProthesisInputData();
-
-	//Setting geometric params for the future 3D model.
-	//See drawing NewProthesis.dwg for help.
-
-
-	/*первый набор параметров*/
-	inputs->a = 26;
-	inputs->b = 19;
-	inputs->c = 7;
-	inputs->d = 45;
-	inputs->e = 42;
-	inputs->f = 28;
-	inputs->g = 13;
-	inputs->h = 57;
-	inputs->i = 49;
-	inputs->j = 21;
-	inputs->k = 17;
-	inputs->l = 28;
-	inputs->m = 11;
-	inputs->n = 7;
-	inputs->o = 7;
-	inputs->p = 6;
-	inputs->q = 2;
-	inputs->r = 5;
-	inputs->s = 5;
-	inputs->t = 15;
-	inputs->u = 22;
-	inputs->v = 6;
-	inputs->w = 6;
-	inputs->x = 13;
-	inputs->y = -13;
-	inputs->z = 7;
-	inputs->a1 = 23;
-	inputs->b1 = 1;
-	inputs->c1 = 10 * 3.14 / 180;
-	inputs->d1 = 90 * 3.14 / 180;
-
-	///*второй набор параметров*/
-	//inputs->a = 26*0.8;
-	//inputs->b = 19*0.8;
-	//inputs->c = 7*0.8;
-	//inputs->d = 45;
-	//inputs->e = 42;
-	//inputs->f = 28;
-	//inputs->g = 15;
-	//inputs->h = 57*1.05;
-	//inputs->i = 49;
-	//inputs->j = 21;
-	//inputs->k = 17;
-	//inputs->l = 28;
-	//inputs->m = 11;
-	//inputs->n = 7;
-	//inputs->o = 7;
-	//inputs->p = 6*0.8;
-	//inputs->q = 2;
-	//inputs->r = 6;
-	//inputs->s = 5;
-	//inputs->t = 15;
-	//inputs->u = 22*1.1;
-	//inputs->v = 6*0.9;
-	//inputs->w = 6;
-	//inputs->x = 13;
-	//inputs->y = -13;
-	//inputs->z = 7*1.1;
-	//inputs->a1 = 23*0.9;
-	//inputs->b1 = 1 * 1.1;
-	//inputs->c1 = 15 * 3.14 / 180;
-	//inputs->d1 = 85 * 3.14 / 180;
-
-	///*третий набор параметров*/
-	//inputs->a = 26 * 1.1;
-	//inputs->b = 19 * 1.1;
-	//inputs->c = 7 * 1.1;
-	//inputs->d = 45;
-	//inputs->e = 42;
-	//inputs->f = 28;
-	//inputs->g = 13;
-	//inputs->h = 57 * 0.9;
-	//inputs->i = 49;
-	//inputs->j = 21;
-	//inputs->k = 17;
-	//inputs->l = 28*1.1;
-	//inputs->m = 11;
-	//inputs->n = 7;
-	//inputs->o = 7;
-	//inputs->p = 6 * 0.8;
-	//inputs->q = 2*0.5;
-	//inputs->r = 4;
-	//inputs->s = 5;
-	//inputs->t = 15;
-	//inputs->u = 22*0.9;
-	//inputs->v = 6*1.1;
-	//inputs->w = 6;
-	//inputs->x = 13;
-	//inputs->y = -13;
-	//inputs->z = 7*1.2;
-	//inputs->a1 = 23*1.1;
-	//inputs->b1 = 1 * 0.9;
-	//inputs->c1 = -10 * 3.14 / 180;
-	//inputs->d1 = 95 * 3.14 / 180;
-
-	///*четвертый набор параметров*/
-	//inputs->a = 26 ;
-	//inputs->b = 19 ;
-	//inputs->c = 7 ;
-	//inputs->d = 45;
-	//inputs->e = 42;
-	//inputs->f = 28;
-	//inputs->g = 13;
-	//inputs->h = 57 ;
-	//inputs->i = 49;
-	//inputs->j = 21;
-	//inputs->k = 17 * 0.5;
-	//inputs->l = 28 ;
-	//inputs->m = 11 * 1.1;
-	//inputs->n = 7;
-	//inputs->o = 7;
-	//inputs->p = 6 ;
-	//inputs->q = 2 ;
-	//inputs->r = 5;
-	//inputs->s = 5;
-	//inputs->t = 15;
-	//inputs->u = 22;
-	//inputs->v = 6;
-	//inputs->w = 6;
-	//inputs->x = 13;
-	//inputs->y = -13;
-	//inputs->z = 7;
-	//inputs->a1 = 23*1.2;
-	//inputs->b1 = 1*1.2;
-	//inputs->c1 = 20 * 3.14 / 180;
-	//inputs->d1 = 90 * 3.14 / 180;
-
-	////head params
-	////coordinates of points
-	//inputs->x1 = 0;
-	//inputs->y1 = 0;
-	//inputs->x2 = 4.92;
-	//inputs->y2 = 6.017;
-	//inputs->x3 = 17;
-	//inputs->y3 = 5.88;
-	//inputs->x4 = 25;
-	//inputs->y4 = 0;
-	////tangent angels of second and third points(degrees)
-	//inputs->tang_ang_deg_2 = 3;
-	//inputs->tang_ang_deg_3 = -3;
-	////head center displacement
-	//inputs->head_center_x_disp = 7.21;
-	//inputs->head_center_y_disp = 25;
-	//inputs->head_center_z_disp = 3;
-	//inputs->head_angle_OX = 20 * 3.14 / 180;
-	//inputs->head_angle_OY = 90 * 3.14 / 180;
-	//inputs->head_angle_OZ = 0;
-
-
-
-
-	//Updating model's geometry information
-	inputs->Update();
-
-	ProthesisBuilder prothesis(inputs);
-	prothesis.LightBuild();
-	//prothesis.visualize();
-	prothesis.WriteToSTLFileLight("C:/FILES/ProthesisBuilder/OUTPUT/Plate.stl");
-	delete inputs;
-	return 0;
-}
+//// MedBox
+//// Prothesis builder program
+//// (c) Victor Borzenkov 2014
+//
+//#include "ProthesisInputData.h"
+//#include "Prothesis.h"
+//
+//int main()
+//{
+//	// Create an object for passing inputs to Prothesis builder class
+//	ProthesisInputData * inputs = new ProthesisInputData();
+//
+//	//Setting geometric params for the future 3D model.
+//	//See drawing NewProthesis.dwg for help.
+//
+//
+//	/*первый набор параметров*/
+//	inputs->a = 26;
+//	inputs->b = 19;
+//	inputs->c = 7;
+//	inputs->d = 45;
+//	inputs->e = 42;
+//	inputs->f = 28;
+//	inputs->g = 13;
+//	inputs->h = 57;
+//	inputs->i = 49;
+//	inputs->j = 21;
+//	inputs->k = 17;
+//	inputs->l = 28;
+//	inputs->m = 11;
+//	inputs->n = 7;
+//	inputs->o = 7;
+//	inputs->p = 6;
+//	inputs->q = 2;
+//	inputs->r = 5;
+//	inputs->s = 5;
+//	inputs->t = 15;
+//	inputs->u = 22;
+//	inputs->v = 6;
+//	inputs->w = 6;
+//	inputs->x = 13;
+//	inputs->y = -13;
+//	inputs->z = 7;
+//	inputs->a1 = 23;
+//	inputs->b1 = 1;
+//	inputs->c1 = 10 * 3.14 / 180;
+//	inputs->d1 = 90 * 3.14 / 180;
+//
+//	///*второй набор параметров*/
+//	//inputs->a = 26*0.8;
+//	//inputs->b = 19*0.8;
+//	//inputs->c = 7*0.8;
+//	//inputs->d = 45;
+//	//inputs->e = 42;
+//	//inputs->f = 28;
+//	//inputs->g = 15;
+//	//inputs->h = 57*1.05;
+//	//inputs->i = 49;
+//	//inputs->j = 21;
+//	//inputs->k = 17;
+//	//inputs->l = 28;
+//	//inputs->m = 11;
+//	//inputs->n = 7;
+//	//inputs->o = 7;
+//	//inputs->p = 6*0.8;
+//	//inputs->q = 2;
+//	//inputs->r = 6;
+//	//inputs->s = 5;
+//	//inputs->t = 15;
+//	//inputs->u = 22*1.1;
+//	//inputs->v = 6*0.9;
+//	//inputs->w = 6;
+//	//inputs->x = 13;
+//	//inputs->y = -13;
+//	//inputs->z = 7*1.1;
+//	//inputs->a1 = 23*0.9;
+//	//inputs->b1 = 1 * 1.1;
+//	//inputs->c1 = 15 * 3.14 / 180;
+//	//inputs->d1 = 85 * 3.14 / 180;
+//
+//	///*третий набор параметров*/
+//	//inputs->a = 26 * 1.1;
+//	//inputs->b = 19 * 1.1;
+//	//inputs->c = 7 * 1.1;
+//	//inputs->d = 45;
+//	//inputs->e = 42;
+//	//inputs->f = 28;
+//	//inputs->g = 13;
+//	//inputs->h = 57 * 0.9;
+//	//inputs->i = 49;
+//	//inputs->j = 21;
+//	//inputs->k = 17;
+//	//inputs->l = 28*1.1;
+//	//inputs->m = 11;
+//	//inputs->n = 7;
+//	//inputs->o = 7;
+//	//inputs->p = 6 * 0.8;
+//	//inputs->q = 2*0.5;
+//	//inputs->r = 4;
+//	//inputs->s = 5;
+//	//inputs->t = 15;
+//	//inputs->u = 22*0.9;
+//	//inputs->v = 6*1.1;
+//	//inputs->w = 6;
+//	//inputs->x = 13;
+//	//inputs->y = -13;
+//	//inputs->z = 7*1.2;
+//	//inputs->a1 = 23*1.1;
+//	//inputs->b1 = 1 * 0.9;
+//	//inputs->c1 = -10 * 3.14 / 180;
+//	//inputs->d1 = 95 * 3.14 / 180;
+//
+//	///*четвертый набор параметров*/
+//	//inputs->a = 26 ;
+//	//inputs->b = 19 ;
+//	//inputs->c = 7 ;
+//	//inputs->d = 45;
+//	//inputs->e = 42;
+//	//inputs->f = 28;
+//	//inputs->g = 13;
+//	//inputs->h = 57 ;
+//	//inputs->i = 49;
+//	//inputs->j = 21;
+//	//inputs->k = 17 * 0.5;
+//	//inputs->l = 28 ;
+//	//inputs->m = 11 * 1.1;
+//	//inputs->n = 7;
+//	//inputs->o = 7;
+//	//inputs->p = 6 ;
+//	//inputs->q = 2 ;
+//	//inputs->r = 5;
+//	//inputs->s = 5;
+//	//inputs->t = 15;
+//	//inputs->u = 22;
+//	//inputs->v = 6;
+//	//inputs->w = 6;
+//	//inputs->x = 13;
+//	//inputs->y = -13;
+//	//inputs->z = 7;
+//	//inputs->a1 = 23*1.2;
+//	//inputs->b1 = 1*1.2;
+//	//inputs->c1 = 20 * 3.14 / 180;
+//	//inputs->d1 = 90 * 3.14 / 180;
+//
+//	////head params
+//	////coordinates of points
+//	//inputs->x1 = 0;
+//	//inputs->y1 = 0;
+//	//inputs->x2 = 4.92;
+//	//inputs->y2 = 6.017;
+//	//inputs->x3 = 17;
+//	//inputs->y3 = 5.88;
+//	//inputs->x4 = 25;
+//	//inputs->y4 = 0;
+//	////tangent angels of second and third points(degrees)
+//	//inputs->tang_ang_deg_2 = 3;
+//	//inputs->tang_ang_deg_3 = -3;
+//	////head center displacement
+//	//inputs->head_center_x_disp = 7.21;
+//	//inputs->head_center_y_disp = 25;
+//	//inputs->head_center_z_disp = 3;
+//	//inputs->head_angle_OX = 20 * 3.14 / 180;
+//	//inputs->head_angle_OY = 90 * 3.14 / 180;
+//	//inputs->head_angle_OZ = 0;
+//
+//
+//
+//
+//	//Updating model's geometry information
+//	inputs->Update();
+//
+//	ProthesisBuilder prothesis(inputs);
+//	prothesis.LightBuild();
+//	//prothesis.visualize();
+//	prothesis.WriteToSTLFileLight("C:/FILES/ProthesisBuilder/OUTPUT/Plate.stl");
+//	delete inputs;
+//	return 0;
+//}
